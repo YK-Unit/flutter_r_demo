4067116e53b0eefce7fba76ed76cc354d3b173b2
York <yorkzhang520@gmail.com>
2019-12-17 13:13:52 +0800

    fix spelling typo bug: 'assert' -> 'asset'

--- lib/R.dart
+++ lib/R.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:r_dart_library/assert_svg.dart';
+import 'package:r_dart_library/asset_svg.dart';
 
 /// This `R` class is generated and contains references to static resources.
 class R {
@@ -13,67 +13,67 @@ class R {
 // ignore: camel_case_types
 class R_Image {
 
-  /// assert: "assets/images/ test blank.png"
+  /// asset: "assets/images/ test blank.png"
   // ignore: non_constant_identifier_names
   static const a_test_blank = AssetImage("assets/images/ test blank.png", package: R.package);
 
-  /// assert: "assets/images/$$test$.png"
+  /// asset: "assets/images/$$test$.png"
   // ignore: non_constant_identifier_names
   static const a$$test$ = AssetImage("assets/images/\$\$test\$.png", package: R.package);
 
-  /// assert: "assets/images/$test.png"
+  /// asset: "assets/images/$test.png"
   // ignore: non_constant_identifier_names
   static const a$test = AssetImage("assets/images/\$test.png", package: R.package);
 
-  /// assert: "assets/images/User@white.png"
+  /// asset: "assets/images/User@white.png"
   // ignore: non_constant_identifier_names
   static const user_white = AssetImage("assets/images/User@white.png", package: R.package);
 
-  /// assert: "assets/images/body_check_done_human.png"
+  /// asset: "assets/images/body_check_done_human.png"
   // ignore: non_constant_identifier_names
   static const body_check_done_human = AssetImage("assets/images/body_check_done_human.png", package: R.package);
 
-  /// assert: "assets/images/body_checking_human.png"
+  /// asset: "assets/images/body_checking_human.png"
   // ignore: non_constant_identifier_names
   static const body_checking_human = AssetImage("assets/images/body_checking_human.png", package: R.package);
 
-  /// assert: "assets/images/btn_cycle_down_80_H.png"
+  /// asset: "assets/images/btn_cycle_down_80_H.png"
   // ignore: non_constant_identifier_names
   static const btn_cycle_down_80_H = AssetImage("assets/images/btn_cycle_down_80_H.png", package: R.package);
 
-  /// assert: "assets/images/btn_cycle_down_80_N.png"
+  /// asset: "assets/images/btn_cycle_down_80_N.png"
   // ignore: non_constant_identifier_names
   static const btn_cycle_down_80_N = AssetImage("assets/images/btn_cycle_down_80_N.png", package: R.package);
 
-  /// assert: "assets/images/5number.png"
+  /// asset: "assets/images/5number.png"
   // ignore: non_constant_identifier_names
   static const a5number = AssetImage("assets/images/5number.png", package: R.package);
 
-  /// assert: "assets/images/_test.png"
+  /// asset: "assets/images/_test.png"
   // ignore: non_constant_identifier_names
   static const a_test = AssetImage("assets/images/_test.png", package: R.package);
 
-  /// assert: "assets/images/icon.ignoreme.png"
+  /// asset: "assets/images/icon.ignoreme.png"
   // ignore: non_constant_identifier_names
   static const icon_ignoreme = AssetImage("assets/images/icon.ignoreme.png", package: R.package);
 
-  /// assert: "assets/images/sameName.gif"
+  /// asset: "assets/images/sameName.gif"
   // ignore: non_constant_identifier_names
   static const sameName_gif = AssetImage("assets/images/sameName.gif", package: R.package);
 
-  /// assert: "assets/images/sameName.jpg"
+  /// asset: "assets/images/sameName.jpg"
   // ignore: non_constant_identifier_names
   static const sameName_jpg = AssetImage("assets/images/sameName.jpg", package: R.package);
 
-  /// assert: "assets/images/sameName.png"
+  /// asset: "assets/images/sameName.png"
   // ignore: non_constant_identifier_names
   static const sameName = AssetImage("assets/images/sameName.png", package: R.package);
 
-  /// assert: "assets/images/test.png"
+  /// asset: "assets/images/test.png"
   // ignore: non_constant_identifier_names
   static const test = AssetImage("assets/images/test.png", package: R.package);
 
-  /// assert: "assets/images/~!$test.png"
+  /// asset: "assets/images/~!$test.png"
   // ignore: non_constant_identifier_names
   static const a__$test = AssetImage("assets/images/~!\$test.png", package: R.package);
 
@@ -83,43 +83,43 @@ class R_Image {
 // ignore: camel_case_types
 class R_Svg {
 
-  /// assert: assets/images/$$test$.svg
+  /// asset: assets/images/$$test$.svg
   // ignore: non_constant_identifier_names
-  static AssertSvg a$$test$({@required double width, @required double height}) {
-    var assertFullPath = "packages/flutter_r_demo/assets/images/\$\$test\$.svg";
-    var imageProvider = AssertSvg(assertFullPath, width: width, height: height);
+  static AssetSvg a$$test$({@required double width, @required double height}) {
+    var assetFullPath = "packages/flutter_r_demo/assets/images/\$\$test\$.svg";
+    var imageProvider = AssetSvg(assetFullPath, width: width, height: height);
     return imageProvider;
   }
 
-  /// assert: assets/images/$test.svg
+  /// asset: assets/images/$test.svg
   // ignore: non_constant_identifier_names
-  static AssertSvg a$test({@required double width, @required double height}) {
-    var assertFullPath = "packages/flutter_r_demo/assets/images/\$test.svg";
-    var imageProvider = AssertSvg(assertFullPath, width: width, height: height);
+  static AssetSvg a$test({@required double width, @required double height}) {
+    var assetFullPath = "packages/flutter_r_demo/assets/images/\$test.svg";
+    var imageProvider = AssetSvg(assetFullPath, width: width, height: height);
     return imageProvider;
   }
 
-  /// assert: assets/images/_test.svg
+  /// asset: assets/images/_test.svg
   // ignore: non_constant_identifier_names
-  static AssertSvg a_test({@required double width, @required double height}) {
-    var assertFullPath = "packages/flutter_r_demo/assets/images/_test.svg";
-    var imageProvider = AssertSvg(assertFullPath, width: width, height: height);
+  static AssetSvg a_test({@required double width, @required double height}) {
+    var assetFullPath = "packages/flutter_r_demo/assets/images/_test.svg";
+    var imageProvider = AssetSvg(assetFullPath, width: width, height: height);
     return imageProvider;
   }
 
-  /// assert: assets/images/manipulation_knead_N.svg
+  /// asset: assets/images/manipulation_knead_N.svg
   // ignore: non_constant_identifier_names
-  static AssertSvg manipulation_knead_N({@required double width, @required double height}) {
-    var assertFullPath = "packages/flutter_r_demo/assets/images/manipulation_knead_N.svg";
-    var imageProvider = AssertSvg(assertFullPath, width: width, height: height);
+  static AssetSvg manipulation_knead_N({@required double width, @required double height}) {
+    var assetFullPath = "packages/flutter_r_demo/assets/images/manipulation_knead_N.svg";
+    var imageProvider = AssetSvg(assetFullPath, width: width, height: height);
     return imageProvider;
   }
 
-  /// assert: assets/images/manipulation_knead_S.svg
+  /// asset: assets/images/manipulation_knead_S.svg
   // ignore: non_constant_identifier_names
-  static AssertSvg manipulation_knead_S({@required double width, @required double height}) {
-    var assertFullPath = "packages/flutter_r_demo/assets/images/manipulation_knead_S.svg";
-    var imageProvider = AssertSvg(assertFullPath, width: width, height: height);
+  static AssetSvg manipulation_knead_S({@required double width, @required double height}) {
+    var assetFullPath = "packages/flutter_r_demo/assets/images/manipulation_knead_S.svg";
+    var imageProvider = AssetSvg(assetFullPath, width: width, height: height);
     return imageProvider;
   }
 
@@ -128,35 +128,35 @@ class R_Svg {
 // ignore: camel_case_types
 class R_Text {
 
-  /// assert: assets/jsons/$$test$.json
+  /// asset: assets/jsons/$$test$.json
   // ignore: non_constant_identifier_names
   static Future<String> a$$test$_json() {
-    var assertFullPath = "packages/flutter_r_demo/assets/jsons/\$\$test\$.json";
-    var str = rootBundle.loadString(assertFullPath);
+    var assetFullPath = "packages/flutter_r_demo/assets/jsons/\$\$test\$.json";
+    var str = rootBundle.loadString(assetFullPath);
     return str;
   }
 
-  /// assert: assets/jsons/test.json
+  /// asset: assets/jsons/test.json
   // ignore: non_constant_identifier_names
   static Future<String> test_json() {
-    var assertFullPath = "packages/flutter_r_demo/assets/jsons/test.json";
-    var str = rootBundle.loadString(assertFullPath);
+    var assetFullPath = "packages/flutter_r_demo/assets/jsons/test.json";
+    var str = rootBundle.loadString(assetFullPath);
     return str;
   }
 
-  /// assert: assets/yamls/$$test$.yaml
+  /// asset: assets/yamls/$$test$.yaml
   // ignore: non_constant_identifier_names
   static Future<String> a$$test$_yaml() {
-    var assertFullPath = "packages/flutter_r_demo/assets/yamls/\$\$test\$.yaml";
-    var str = rootBundle.loadString(assertFullPath);
+    var assetFullPath = "packages/flutter_r_demo/assets/yamls/\$\$test\$.yaml";
+    var str = rootBundle.loadString(assetFullPath);
     return str;
   }
 
-  /// assert: assets/yamls/test.yaml
+  /// asset: assets/yamls/test.yaml
   // ignore: non_constant_identifier_names
   static Future<String> test_yaml() {
-    var assertFullPath = "packages/flutter_r_demo/assets/yamls/test.yaml";
-    var str = rootBundle.loadString(assertFullPath);
+    var assetFullPath = "packages/flutter_r_demo/assets/yamls/test.yaml";
+    var str = rootBundle.loadString(assetFullPath);
     return str;
   }
 
